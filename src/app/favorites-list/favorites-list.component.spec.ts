1560579e78a9fbc587106c684a623f3b7901a429
Sam <samleo0823@gmail.com>
2020-04-16 22:50:15 -0500

    added some style to favorites

--- src/app/favorites-list/favorites-list.component.spec.ts
+++ src/app/favorites-list/favorites-list.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DirectoryComponent } from 'src/app/directory/directory.component';
 import { FavoritesListComponent } from './favorites-list.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Character } from '../character';
 
 describe('FavoritesListComponent', () => {
   let component: FavoritesListComponent;
+  let dComponent: DirectoryComponent;
   let fixture: ComponentFixture<FavoritesListComponent>;
+  let directoryFixture: ComponentFixture<DirectoryComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FavoritesListComponent ]
+      declarations: [ FavoritesListComponent, DirectoryComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   }));
@@ -22,4 +27,16 @@ describe('FavoritesListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should show added character name in list when Add To Favorites button is clicked', () => {
+    directoryFixture = TestBed.createComponent(DirectoryComponent);
+    dComponent.ngOnInit();
+    dComponent = directoryFixture.componentInstance;
+    dComponent.selectedCharacter = {id: 1, name: "Rick Sanchez", status: '', species: "Human", type: "", gender: "Male", 
+    origin: {}, location: {}, image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg", episode: [], url: '', created: '',
+    selected: true
+    };
+    dComponent.sendSelectedCharacterToFavList();
+    expect(component.favCharacters[0].name === "Rick Sanchez");
+  })
 });
--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -27,6 +27,6 @@ describe('HomePageComponent', () => {
     const fixture = TestBed.createComponent(HomePageComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('div.img-container>img').src).toContain('/assets/logo2.png');
+    expect(compiled.querySelector('div.img-container>img').src).toContain('https://bubbleblabber-wpengine.netdna-ssl.com/wp-content/uploads/2015/06/RAM_LOGO_FINAL.jpg');
   }));
 });
